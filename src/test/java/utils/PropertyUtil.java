98903cdecd6a65ea265e217dbb20d8e6f758621d
Lishko Dmitry <d.lishko@a1qa.com>
2019-10-30 14:50:20 +0300

    add property util

--- src/test/java/utils/PropertyUtil.java
+++ src/test/java/utils/PropertyUtil.java
@@ -1,4 +1,35 @@
 package utils;
 
+import org.testng.ReporterConfig;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertyUtil {
+    public static final String CONFIG_PROPERTIES = "src/test/resources/config.properties";
+    public static final String BROWSER_CONFIG_FIELD = "browser.name";
+    public static final String USERNAME_CONFIG_FIELD = "username";
+    public static final String PASSWORD_CONFIG_FIELD = "password";
+
+    private Properties property;
+
+    public PropertyUtil() {
+        FileInputStream inputStream;
+        property = new Properties();
+        try {
+            inputStream = new FileInputStream(CONFIG_PROPERTIES);
+            property.load(inputStream);
+        } catch (IOException e) {
+            System.err.println("File don't find.");
+        }
+    }
+
+    public String getBrowserType() {
+        return property.getProperty(BROWSER_CONFIG_FIELD);
+    }
+
+    public String[] getUsernameAndPassword() {
+        return new String[]{property.getProperty(USERNAME_CONFIG_FIELD), property.getProperty(PASSWORD_CONFIG_FIELD)};
+    }
 }
