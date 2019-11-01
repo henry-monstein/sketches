75e015d5d779649bacc2f22216406f8814ed12a0
Henry Monstein <1268620@gmail.com>
2019-11-02 02:32:02 +0300

    add fluent wait

--- src/test/java/utils/Waiter.java
+++ src/test/java/utils/Waiter.java
@@ -3,8 +3,13 @@ package utils;
 import browser.Browser;
 import constants.TimeOutConstant;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Waiter {
 
@@ -17,5 +22,11 @@ public class Waiter {
     public static void waitPresence(By locator) {
         new WebDriverWait(Browser.getDriver(), TimeOutConstant.getPresenceOfElementTimeout())
                 .until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public static void fluentWait(By locator) {
+        Wait<WebDriver> wait = new FluentWait<>(Browser.getDriver())
+                .withTimeout(Duration.ofSeconds(TimeOutConstant.getClickableTimeout()));
+        wait.until(driver -> Browser.getDriver().findElement(locator));
     }
 }
